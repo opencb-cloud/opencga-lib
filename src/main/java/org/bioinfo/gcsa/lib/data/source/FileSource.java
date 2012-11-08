0bf210b65456825d8d1d8c8bc9102a41048c98c2
imedina <imedina@cipf.es>
2012-11-08 17:35:35 +0100

    data packeged cerated

---
+++ src/main/java/org/bioinfo/gcsa/lib/data/source/FileSource.java
@@ -0,0 +1,27 @@
+package org.bioinfo.gcsa.lib.data.source;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+public class FileSource implements Source {
+
+	@Override
+	public InputStream getInputStream(String path) {
+		try {
+			return new FileInputStream(new File(path));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
