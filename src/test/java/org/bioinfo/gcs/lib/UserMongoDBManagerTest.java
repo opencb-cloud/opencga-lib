5744fb46cde5ba1a1ae7a8b3ebd8afb0bbd5047f
Francisco Salavert Torres <fsalavert@cipf.es>
2012-11-28 14:37:08 +0100

    test

--- src/test/java/org/bioinfo/gcs/lib/UserMongoDBManagerTest.java
+++ src/test/java/org/bioinfo/gcs/lib/UserMongoDBManagerTest.java
@@ -20,7 +20,7 @@ public class UserMongoDBManagerTest {
 				CloudSessionManager cloudSessionManager = new CloudSessionManager(System.getenv("GCSA_HOME"));
 				userManager = cloudSessionManager.getUserManager();
 				
-				String sessionId = "2kPfNpHqYgRoMloMBuqf";
+				String sessionId = "c33HbnyKp2EWnRxVSCkq";
 				File f = new File("/home/examples/bam/HG00096.chrom20.ILLUMINA.bwa.GBR.exome.20111114.bam");
 				String data = "sampletext";
 				InputStream fileData = new FileInputStream(f);
