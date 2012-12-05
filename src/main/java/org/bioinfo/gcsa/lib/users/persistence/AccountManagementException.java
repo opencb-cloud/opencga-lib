e6de48b86666754f30872c5f0f33e45235ee4d15
Francisco Salavert Torres <fsalavert@cipf.es>
2012-12-05 20:18:05 +0100

    refactor user to account

---
+++ src/main/java/org/bioinfo/gcsa/lib/users/persistence/AccountManagementException.java
@@ -0,0 +1,11 @@
+package org.bioinfo.gcsa.lib.users.persistence;
+
+
+public class AccountManagementException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AccountManagementException(String msg) {
+		super(msg);
+	}
+}
