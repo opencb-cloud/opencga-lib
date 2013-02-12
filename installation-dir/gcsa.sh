eb56a6df8bc33b6cc4a3cf10e3ca17c595c0ffe7
rsanchez <rsanchez@rsanchez.(none)>
2013-02-12 17:46:28 +0100

    Local mode: added methods

--- installation-dir/gcsa.sh
+++ installation-dir/gcsa.sh
@@ -22,15 +22,22 @@ then
 	exit 1
 fi
 
-if [ $# -eq 0 ]
-then
-	echo "Arguments expected."
-	exit 1
-fi
+#if [ $# -eq 0 ]
+#then
+#	echo "Arguments expected."
+#	exit 1
+#fi
 
 for i in $GCSA_HOME/libs/*; do
 	CLASSPATH=$CLASSPATH:$i
 done;
 
-java $OPTIONS -classpath $CLASSPATH org.bioinfo.gcsa.lib.cli.GcsaMain $@ --home $GCSA_HOME
+echo "*************************"
+echo "*************************"
+echo "*** GCSA Local server ***"
+echo "*************************"
+echo "*************************"
+echo "The URL address to access the server is like this: http://localhost:{PORT}/gcsa/rest/storage/fetch?filepath={ABSOLUTE FILE PATH}&region={REGION}"
+
+java $OPTIONS -classpath $CLASSPATH org.bioinfo.gcsa.lib.cli.GcsaMain $GCSA_HOME $@ 2> /dev/null
 
--- src/main/java/org/bioinfo/gcsa/lib/cli/GcsaMain.java
+++ src/main/java/org/bioinfo/gcsa/lib/cli/GcsaMain.java
@@ -1,40 +1,46 @@
 package org.bioinfo.gcsa.lib.cli;
 
 import java.io.File;
+import java.util.Properties;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.commons.cli.Options;
-
+import org.bioinfo.gcsa.Config;
 
 public class GcsaMain {
 
-	public static Tomcat tomcat; 
-	protected Options options;
+	public static Tomcat tomcat;
 
 	public static void main(String[] args) {
 
+		String home = args[0];
+		if (home == null || home.trim().equals("")) {
+			home = ".";
+		}
+
+		Properties properties = Config.getLocalServerProperties(home);
+		int port = Integer.parseInt(properties.getProperty("LOCAL.PORT", "33333"));
+
 		tomcat = new Tomcat();
-		tomcat.setPort(33333);  
+		tomcat.setPort(port);
 
-		Context ctx = tomcat.addContext("/gcsa/rest/storage", new File(".").getAbsolutePath());  
+		Context ctx = tomcat.addContext("/gcsa/rest", new File(".").getAbsolutePath());
 
-		Tomcat.addServlet(ctx, "fetch", new FetchServlet());  
-		ctx.addServletMapping("/fetch", "fetch");  
+		Tomcat.addServlet(ctx, "fetch", new FetchServlet());
+		ctx.addServletMapping("/storage/fetch", "fetch");
 
-		Tomcat.addServlet(ctx, "admin", new FetchServlet());  
-		ctx.addServletMapping("/admin", "admin"); 
-		
+		Tomcat.addServlet(ctx, "admin", new AdminServlet());
+		ctx.addServletMapping("/admin", "admin");
+
 		try {
 			tomcat.start();
 			tomcat.getServer().await();
-			
 		} catch (LifecycleException e) {
 			e.printStackTrace();
-		}  
+		}
 	}
-	
+
 	public static void stop() throws LifecycleException {
 		tomcat.stop();
 	}
