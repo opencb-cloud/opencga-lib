cf319ee83e5c57d5dea26c84c3b40b8b5990a0e8
Francisco Salavert Torres <fsalavert@cipf.es>
2013-02-19 16:07:19 +0100

    VcfManager added

---
+++ src/test/java/org/bioinfo/gcs/lib/VcfManagerTest.java
@@ -0,0 +1,37 @@
+package org.bioinfo.gcs.lib;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bioinfo.gcsa.lib.account.io.IOManagementException;
+import org.bioinfo.gcsa.lib.storage.feature.BamManager;
+import org.bioinfo.gcsa.lib.storage.feature.VcfManager;
+import org.junit.Test;
+
+public class VcfManagerTest {
+
+	@Test
+	public void test() throws IOManagementException {
+		if(new File("/home/examples").exists()){
+			try {
+				VcfManager man = new VcfManager();
+				Path file = Paths.get("/home","examples","vcf","1000genomes_5000_variants.vcf.gz");
+				Map<String, List<String>> params = new HashMap<String, List<String>> ();
+				String result = man.getByRegion(file,"19:246522-246722", params );
+				System.out.println(result);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else{
+			System.out.println("/home/examples not found, skip.");
+		}
+	}
+
+}
